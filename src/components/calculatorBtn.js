bd9d61f60a1bc58903f30a00d6de02b07af76ff2
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-11-23 11:11:57 +0200

    Add class component for CalculatorBtn.js

---
+++ src/components/calculatorBtn.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Prop-types from 'prop-types';
+
+class CalculatorBtn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
